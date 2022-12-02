5976416bb4a93d64af15e0f6ca7e635b52ecbfd3
Adrian Wu <eattofueveryday@gmail.com>
2022-12-02 08:55:35 -0500

    day 2

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,6 +1,10 @@
+#![allow(dead_code)]
+
 mod s0;
 mod s1;
+mod s2;
+mod s3;
 
 fn main() {
-  s1::main()
+  s3::main()
 }
---
+++ 2022/src/s3.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
+use bstr::{BString, ByteSlice, ByteVec};
+
+pub fn main() {
+  let mut input = BString::default();
+  File::open("input.txt").unwrap().read_to_end(input.as_vec_mut()).unwrap();
+  let score: i64 = input.split_str("\n")
+    .map(|v| v.split_once_str(" ").unwrap())
+    .map(|v| (*v.0.get(0usize).unwrap(), *v.1.get(0usize).unwrap()))
+    .map(|v| {
+      let lut = HashMap::from([
+        ((b'A', b'X'), 0+3),
+        ((b'B', b'X'), 0+1),
+        ((b'C', b'X'), 0+2),
+        ((b'A', b'Y'), 3+1),
+        ((b'B', b'Y'), 3+2),
+        ((b'C', b'Y'), 3+3),
+        ((b'A', b'Z'), 6+2),
+        ((b'B', b'Z'), 6+3),
+        ((b'C', b'Z'), 6+1)
+      ]);
+      *lut.get(&v).unwrap()
+    })
+    .sum();
+  println!("{}", score);
+}
