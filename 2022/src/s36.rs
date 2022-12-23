562607fec79cb8d42d48c3a73bd7648872a01619
Adrian Wu <eattofueveryday@gmail.com>
2022-12-23 15:26:23 -0500

    Day 19 Opt: i64->i16 to optimize memory use

--- 2022/src/s36.rs
+++ 2022/src/s36.rs
@@ -7,7 +7,7 @@ use crate::util::input;
 
 const DEBUG: bool = false;
 
-const MAX_TIME: i64 = 24;
+const MAX_TIME: i16 = 24;
 
 const B_ORE_BOT_ORE: usize = 0;
 const B_CLAY_BOT_ORE: usize = 1;
@@ -16,7 +16,7 @@ const B_OBBI_BOT_CLAY: usize = 3;
 const B_GEODE_BOT_ORE: usize = 4;
 const B_GEODE_BOT_OBBI: usize = 5;
 
-type Blueprint = [i64; 6];
+type Blueprint = [i16; 6];
 
 const N_ORE: usize = 0;
 const N_ORE_BOTS: usize = 1;
@@ -27,7 +27,7 @@ const N_OBBI_BOTS: usize = 5;
 const N_GEODE: usize = 6;
 const N_GEODE_BOTS: usize = 7;
 
-type Node = [i64; 8];
+type Node = [i16; 8];
 
 const START_NODE: Node = [0, 1, 0, 0, 0, 0, 0, 0];
 
@@ -36,7 +36,7 @@ fn add_nodes(b: Node, d: Node) -> Node {
 }
 
 #[derive(PartialEq, Copy, Clone)]
-struct ScoredNode(pub Node, pub f64);
+struct ScoredNode(pub Node, pub f32);
 
 impl Eq for ScoredNode { }
 
@@ -57,7 +57,7 @@ impl PartialOrd for ScoredNode {
 mod heuristics {
   use crate::s36::{N_GEODE, N_GEODE_BOTS, Node};
 
-  pub fn default(node: Node, dest_geodes: i64) -> f64 {
+  pub fn default(node: Node, dest_geodes: i16) -> f32 {
     projected_geodes(node, dest_geodes)
   }
 
@@ -65,19 +65,19 @@ mod heuristics {
     0.0
   }
 
-  pub fn projected_geodes(node: Node, dest_geodes: i64) -> f64 {
+  pub fn projected_geodes(node: Node, dest_geodes: i16) -> f32 {
     // geodes = (num_bots + t / 2) * t
     // 0 = (1/2)*t^2 + num_bots*t - geodes
     // t = sqrt(num_bots^2 + 2*geodes)-num_bots
     let needed_geodes = dest_geodes - node[N_GEODE];
-    ((node[N_GEODE_BOTS].pow(2) + 2 * needed_geodes) as f64).sqrt() - node[N_GEODE_BOTS] as f64
+    ((node[N_GEODE_BOTS].pow(2) + 2 * needed_geodes) as f32).sqrt() - node[N_GEODE_BOTS] as f32
   }
 }
 
-fn a_star(blueprint: Blueprint, start_node: Node, end_geodes: i64) -> i64 {
+fn a_star(blueprint: Blueprint, start_node: Node, end_geodes: i16) -> i16 {
   let mut pq = BinaryHeap::new();
   pq.push(ScoredNode(start_node, 0.0));
-  let mut g_score = HashMap::<Node, i64>::new();
+  let mut g_score = HashMap::<Node, i16>::new();
   let mut prev = HashMap::new();
   g_score.insert(start_node, 0);
   while !pq.is_empty() {
@@ -128,18 +128,18 @@ fn a_star(blueprint: Blueprint, start_node: Node, end_geodes: i64) -> i64 {
               panic!("Heuristic returned NaN");
             }
           }
-          pq.push(ScoredNode(v, maybe_g_score as f64 + h_score));
+          pq.push(ScoredNode(v, maybe_g_score as f32 + h_score));
         }
       });
   }
   panic!("destination unreachable")
 }
 
-fn thread_main(blueprint: Blueprint) -> i64 {
+fn thread_main(blueprint: Blueprint) -> i16 {
   let mut upper;
-  let mut lower = 0i64;
+  let mut lower = 0;
   // discover upper bound
-  let mut tentative_upper_bound = 16i64;
+  let mut tentative_upper_bound = 16;
   loop {
     let time = a_star(blueprint, START_NODE, tentative_upper_bound);
     if time > MAX_TIME {
@@ -167,7 +167,7 @@ fn thread_main(blueprint: Blueprint) -> i64 {
 fn parse_input() -> Vec<Blueprint> {
   input().split_str("\n")
     .map(|v| v.split_str(" ")
-      .map(|v| v.to_str_lossy().parse::<i64>())
+      .map(|v| v.to_str_lossy().parse::<i16>())
       .collect::<Vec<_>>())
     .map(|v| [*v[6].as_ref().unwrap(), *v[12].as_ref().unwrap(), *v[18].as_ref().unwrap(), *v[21].as_ref().unwrap(), *v[27].as_ref().unwrap(), *v[30].as_ref().unwrap()])
     .collect::<Vec<_>>()
@@ -181,14 +181,9 @@ fn real_main() {
     .collect::<Vec<_>>()
     .into_iter()
     .enumerate()
-    .map(|v| (v.0 + 1) as i64 * v.1)
+    .map(|v| (v.0 + 1) as i64 * v.1 as i64)
     .sum::<i64>();
   println!("{}", ans);
-}
-
-fn test_main() {
-  let input = parse_input();
-  println!("{}", a_star(input[0], START_NODE, 9));
 }
 
 pub fn main() {
